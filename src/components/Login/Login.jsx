d2bec4dce2fa488fde096b0cd531bb9d649885c2
Md. Hasanur Rahman <hasanurhasu127@gmail.com>
2023-10-01 11:29:45 +0600

    added github

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,37 +1,67 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 import app from "../../firebase/firebase.init";
 import { useState } from "react";
-
 const Login = () => {
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState(null)
+
     const auth = getAuth(app);
-    console.log(app);
     const provider = new GoogleAuthProvider();
+    const gitHubProvider = new GithubAuthProvider();
+
     const handleGoogleSingIn = () => {
         signInWithPopup(auth, provider)
             .then(result => {
-                const loggedUser = result.user;
-                setUser(loggedUser)
-
+                const loggerUser = result.user;
+                setUser(loggerUser)
             })
             .catch(error => {
-                console.log(error.message);
+                console.log(error);
+            })
+    }
+    const handleSingOut = () => {
+        signOut(auth)
+            .then(result => {
+                setUser(null)
+                console.log(result);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+    const handleGitHubSingIn = () => {
+        signInWithPopup(auth, gitHubProvider)
+            .then(result => {
+                const giHubLoggedUser = result.user;
+                setUser(giHubLoggedUser);
+                console.log(giHubLoggedUser);
+            })
+            .catch(error => {
+                console.log(error);
             })
     }
     console.log(user);
     return (
         <div>
-            <div className="flex justify-center mt-5">
-                <button
-                    onClick={handleGoogleSingIn}
-                    className="text-center py-2 bg-green-700 px-4 rounded-md text-white">Google login
-                </button>
+
+
+            <div className="flex justify-center mt-6 gap-5">
+                {
+                    user ? <button onClick={handleSingOut} className="py-2 px-4 bg-green-500 text-white text-lg font-bold rounded-md">Sing Out</button>
+                        :
+                        <>
+                            <button onClick={handleGoogleSingIn} className="py-2 px-4 bg-green-500 text-white text-lg font-bold rounded-md">Google Sing In</button>
+                            <button onClick={handleGitHubSingIn} className="py-2 px-4 bg-green-500 text-white text-lg font-bold rounded-md">GitHub Sing In</button>
+                        </>
+                }
             </div>
-            <div className="">
-                <h1>{user?.displayName}</h1>
-                <p>{user?.email}</p>
-                <img src={user?.photoURL} alt="" />
-            </div>
+            {
+                user &&
+                <div className="flex flex-col items-center">
+                    <h1 className="text-lg font-semibold">{user.displayName}</h1>
+                    <p>Email: {user.email}</p>
+                    <img src={user.photoURL} alt="" />
+                </div>
+            }
         </div>
     );
 };
